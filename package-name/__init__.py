136727a4bf0bdc424cb0be74ca4dd9996abae53a
SquirrelStore <130902301+SquirrelStore@users.noreply.github.com>
2024-10-26 22:26:27 +0800

    Revert "Archive (#1)"
    This reverts commit b02211d77f9359b0453809af19b1fe33cd4dcf54.

--- package-name/__init__.py
+++ package-name/__init__.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2024 Ahzyuan. - All Rights Reserved
+Copyright (C) 2024 <your-name>. - All Rights Reserved
  * You may use, distribute and modify this code under the terms of the <license-name> license.
  * You should have received a copy of the <license-name> license with this file.
  * If not, please visit <full_text-url-of-license-terms> for more information.
@@ -7,6 +7,4 @@ Copyright (C) 2024 Ahzyuan. - All Rights Reserved
 
 __version__ = '0.1.0'
 
-from .greet import greet
-
-__all__ = ['greet']
+from .core import *
